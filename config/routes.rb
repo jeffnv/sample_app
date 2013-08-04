e4ea1542da113f7494a632bca2f0073c1b94c605
Jeffrey Fiddler <jeffnv@gmail.com>
2013-08-04 13:58:08 -0700

    CSS things up a bit. name routes. refactor tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root              to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  #   match 'products/:id'                          => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  #   match 'products/:id/purchase'                 => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
@@ -41,7 +40,7 @@ SampleApp::Application.routes.draw do
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get 'recent', :on => :collection
+  #       get 'recent', :on                         => :collection
   #     end
   #   end
 
@@ -54,7 +53,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to                                        => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
