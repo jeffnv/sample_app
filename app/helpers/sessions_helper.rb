b9d9c90a4875ebef78088e03c8e43876873a4bf3
Jeffrey Fiddler <jeffnv@gmail.com>
2013-08-19 19:18:15 -0700

    add framework for making microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,6 +38,16 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url
   end
-    
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
   
 end
