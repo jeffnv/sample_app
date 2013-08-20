89be9e6f0c4909443e75dcb0aa2257c2168bd92d
Jeffrey Fiddler <jeffnv@gmail.com>
2013-08-20 05:47:13 -0700

    add web interface for microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,15 @@ class MicropostsController < ApplicationController
   def index
   end
 
+
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
