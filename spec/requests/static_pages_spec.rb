5e370b207d5d60731849d46a5191a8b55a5e25c3
Jeffrey Fiddler <jeffnv@gmail.com>
2013-08-25 16:33:47 -0700

    add follow/unfollow buttons to profiles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,8 +18,6 @@ describe "Static pages" do
     
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
-      
-      
       before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
@@ -32,6 +30,18 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+      
       
       it "should have the correct micropost count and pluralization" do
         should have_content "#{user.microposts.count} microposts" 
